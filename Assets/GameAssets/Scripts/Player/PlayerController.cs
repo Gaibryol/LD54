3641e0f77fa10d44d6cf9d3a7b22f4857003c04b
Gaibryol <gaibryol@gmail.com>
2023-09-29 22:00:45 -0600

    Created game manager and ui stuff with some player code

--- Assets/GameAssets/Scripts/Player/PlayerController.cs
+++ Assets/GameAssets/Scripts/Player/PlayerController.cs
@@ -12,6 +12,10 @@ public class PlayerController : MonoBehaviour
 	private InputAction move;
 	private InputAction jump;
 
+	private bool grounded;
+
+	private readonly EventBrokerComponent eventBroker = new EventBrokerComponent();
+
 	[SerializeField] private float moveSpeed;
 	[SerializeField] private float jumpStrength;
 
@@ -20,11 +24,17 @@ public class PlayerController : MonoBehaviour
 		rbody = GetComponent<Rigidbody2D>();
 		coll = GetComponent<CapsuleCollider2D>();
 		playerControls = new PlayerControls();
+
+		grounded = false;
 	}
 
 	private void Jump(InputAction.CallbackContext context)
 	{
-		rbody.AddForce(new Vector2(0, jumpStrength), ForceMode2D.Impulse);
+		if (grounded)
+		{
+			rbody.AddForce(new Vector2(0, jumpStrength), ForceMode2D.Impulse);
+			return;
+		}
 	}
 
 	private void OnEnable()
@@ -49,5 +59,25 @@ public class PlayerController : MonoBehaviour
 	{
 		Vector2 moveDirection = move.ReadValue<Vector2>();
 		rbody.velocity = new Vector2(moveDirection.x * moveSpeed, rbody.velocity.y);
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.transform.tag == Constants.GroundTag && Mathf.Abs(collision.GetContact(0).point.y - (transform.position.y - (coll.bounds.size.y * 0.5f))) <= Constants.Player.AllowedCollisionMargin)
+		{
+			grounded = true;
+		}
+		else if (Mathf.Abs(collision.GetContact(0).point.y - (transform.position.y + (coll.bounds.size.y * 0.5f))) <= Constants.Player.AllowedCollisionMargin)
+		{
+			eventBroker.Publish(this, new GameStateEvents.EndGame());
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		if (collision.transform.tag == Constants.GroundTag)
+		{
+			grounded = false;
+		}
 	}
 }
---
+++ Assets/GameAssets/Scripts/Systems/UIManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+	private readonly EventBrokerComponent eventBroker = new EventBrokerComponent();
+
+	private GameManager gameManager;
+
+	[SerializeField] private TMP_Text scoreText;
+
+	[SerializeField, Header("End Screen")] private GameObject endScreen;
+	[SerializeField] private TMP_Text finalScoreText;
+	[SerializeField] private Button restartButton;
+
+	private void Awake()
+	{
+
+	}
+
+	private void FixedUpdate()
+	{
+		scoreText.text = gameManager.Score.ToString();
+	}
+
+	private void StartGameHandler(BrokerEvent<GameStateEvents.StartGame> inEvent)
+	{
+		gameManager = inEvent.Payload.GameManager;
+	}
+
+	private void EndGameHandler(BrokerEvent<GameStateEvents.EndGame> inEvent)
+	{
+		endScreen.SetActive(true);
+		finalScoreText.text = gameManager.Score.ToString();
+	}
+
+	private void RestartGame()
+	{
+		eventBroker.Publish(this, new GameStateEvents.RestartGame());
+		endScreen.SetActive(false);
+	}
+
+	private void OnEnable()
+	{
+		eventBroker.Subscribe<GameStateEvents.StartGame>(StartGameHandler);
+		eventBroker.Subscribe<GameStateEvents.EndGame>(EndGameHandler);
+
+		restartButton.onClick.AddListener(RestartGame);
+	}
+
+	private void OnDisable()
+	{
+		eventBroker.Unsubscribe<GameStateEvents.StartGame>(StartGameHandler);
+		eventBroker.Unsubscribe<GameStateEvents.EndGame>(EndGameHandler);
+
+		restartButton.onClick.RemoveListener(RestartGame);
+	}
+}
