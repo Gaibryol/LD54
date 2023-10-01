3bd5df9fa4520934218ce67f0da9b53e4da9c8f8
akchen1 <43446512+akchen1@users.noreply.github.com>
2023-10-01 15:11:35 -0600

    change from world position to num blocks

--- Assets/GameAssets/Scripts/Tetris/TetrisPieces.cs
+++ Assets/GameAssets/Scripts/Tetris/TetrisPieces.cs
@@ -31,6 +31,7 @@ public class TetrisPieces
 		int index = UnityEngine.Random.Range(0, spawningBag.Count);
         int pieceType = spawningBag[index];
         spawningBag.RemoveAt(index);
+        pieceType = 0;
         switch (pieceType)
         {
             case 0:
