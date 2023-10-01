b8312daeda2aa40c859095ededbc7bd27192aac5
Gaibryol <gaibryol@gmail.com>
2023-09-30 18:45:37 -0600

    Added audio system and set up without the tracks

---
+++ Assets/GameAssets/Scripts/Constants/Constants+Audio.cs
@@ -0,0 +1,22 @@
+
+public partial class Constants
+{
+	public class Audio
+	{
+		public const float MusicFadeSpeed = 2.5f;
+		public const float DefaultAudioLevel = 0.25f;
+
+		public const string MusicVolumePP = "MusicVolume";
+		public const string SFXVolumePP = "SFXVolume";
+
+		public struct Music
+		{
+			public const string MainMenu = "MainMenu";
+		}
+
+		public struct SFX
+		{
+			public const string Button = "Button";
+		}
+	}
+}
