685aa692a2f4b692434d925830ab794dad7cc3e8
akchen1 <43446512+akchen1@users.noreply.github.com>
2023-10-01 22:52:46 -0600

    inidcator

--- Assets/GameAssets/Scripts/Tetris/Guide.cs
+++ Assets/GameAssets/Scripts/Tetris/Guide.cs
@@ -7,8 +7,15 @@ using UnityEngine;
 public class Guide : MonoBehaviour
 {
     [SerializeField] private Block blockTemplate;
-    [SerializeField] private Transform guideLine;
+    [SerializeField] private Transform centerGuideLine;
+
+    [SerializeField, Header("Prefabs")] private GameObject centerWithBorder;
+    [SerializeField] private GameObject centerNoBorder;
+    [SerializeField] private GameObject leftBorder;
+    [SerializeField] private GameObject rightBorder;
+
     List<Block> blocks = new List<Block>();
+    List<GameObject> spawnedGuideLines = new List<GameObject>();
 
     private PieceTemplate pieceTemplate;
     EventBrokerComponent eventBrokerComponent = new EventBrokerComponent();
@@ -37,7 +44,10 @@ public class Guide : MonoBehaviour
         {
             constrainedPosition.y = pieceTemplate.blocksLeftOfCenter;
         }
-        guideLine.localPosition = new Vector3(position.y * TetrisConstants.BLOCK_SIZE, guideLine.transform.localPosition.y, guideLine.transform.localPosition.z);
+
+        float xPos = position.y * TetrisConstants.BLOCK_SIZE - TetrisConstants.BLOCK_SIZE / 2 - TetrisConstants.BLOCK_SIZE * pieceTemplate.blocksLeftOfCenter;
+        xPos = Mathf.Clamp(xPos, -TetrisConstants.BLOCK_SIZE / 2, (TetrisConstants.BLOCK_SIZE * TetrisConstants.COLS) - (TetrisConstants.BLOCK_SIZE / 2) - (TetrisConstants.BLOCK_SIZE * (spawnedGuideLines.Count)));
+        centerGuideLine.localPosition = new Vector3(xPos, centerGuideLine.transform.localPosition.y, centerGuideLine.transform.localPosition.z);
         transform.localPosition = new Vector3((constrainedPosition.y - pieceTemplate.pieceCenter.y) * TetrisConstants.BLOCK_SIZE, transform.localPosition.y, transform.localPosition.z);
     }
 
@@ -55,6 +65,45 @@ public class Guide : MonoBehaviour
         {
             block.transform.localPosition += (Vector3)pieceTemplate.guidePreviewOffset * TetrisConstants.BLOCK_SIZE;
         }
+        DestroyGuideLines();
+        SpawnGuideLines();
+    }
+
+    private void DestroyGuideLines()
+    {
+        foreach (GameObject guide in spawnedGuideLines)
+        {
+            Destroy(guide);
+        }
+        spawnedGuideLines.Clear();
+    }
+
+    private void SpawnGuideLines()
+    {
+        int width = 1 + pieceTemplate.blocksLeftOfCenter + pieceTemplate.blockRightOfCenter;
+        if (width == 1)
+        {
+            GameObject guideLine = Instantiate(centerWithBorder, centerGuideLine);
+            guideLine.transform.localPosition = new Vector3(TetrisConstants.BLOCK_SIZE/2, 0, 0);
+            spawnedGuideLines.Add(guideLine);
+            return;
+        }
+
+        GameObject guideLineLeft = Instantiate(leftBorder, centerGuideLine);
+        guideLineLeft.transform.localPosition = new Vector3(TetrisConstants.BLOCK_SIZE/2, 0, 0);
+        GameObject guideLineRight = Instantiate(rightBorder, centerGuideLine);
+        guideLineRight.transform.localPosition = new Vector3(TetrisConstants.BLOCK_SIZE * (width - 1) + TetrisConstants.BLOCK_SIZE / 2, 0, 0);
+
+        for (int i = 1; i < width - 1; i++)
+        {
+            GameObject guideLineMiddle = Instantiate(centerNoBorder, centerGuideLine);
+            guideLineMiddle.transform.localPosition = new Vector3((TetrisConstants.BLOCK_SIZE) * i + TetrisConstants.BLOCK_SIZE / 2, 0, 0);
+            spawnedGuideLines.Add(guideLineMiddle);
+        }
+
+        spawnedGuideLines.Add(guideLineLeft);
+        spawnedGuideLines.Add(guideLineRight);
+        
     }
 
     private Vector2Int MapPlayerLocation()
